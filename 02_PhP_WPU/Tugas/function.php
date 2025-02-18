9248bc3d913f5f89e0f293b7fe4527c2ad53fb55
hanizakitul <hanizakiatulaminah@gmail.com>
2025-02-18 16:32:06 +0700

    baru

---
+++ 02_PhP_WPU/Tugas/function.php
@@ -0,0 +1,38 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "form pengaduan");
+
+
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while( $row = mysqli_fetch_array($result) ) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data) {
+    global $conn;
+
+    $nama = $_POST["nama"];
+    $pengaduan = $_POST["pengaduan"];
+    $gambar = $_POST["gambar"]; 
+
+    $query = "INSERT INTO formmahasiswa
+    VALUES
+    ('', '$nama', '$pengaduan', '$gambar')
+    ";
+mysqli_query($conn, $query);
+
+return mysqli_affected_rows($conn);
+
+}
+
+function hapus($id) {
+    global $conn;
+    mysqli_query($conn, "DELETE FROM frommahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+?>
---
+++ 02_PhP_WPU/Tugas/tambah.php
@@ -0,0 +1,49 @@
+<?php
+// koneksi ke DBMS
+$conn = mysqli_connect("localhost", "root", "", "formmahasiswa");
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+    // ambil data dari tiap elemen dalam form
+    $nama = $_POST["nama"];
+    $pengaduan = $_POST["pengaduan"];
+    $gambar = $_POST["gambar"];
+
+    // query insert data
+    $query = "INSERT INTO formmahasiswa
+                VALUES
+                ";
+    mysqli_query($conn,$query);
+    
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tambah data mahasiswa</title>
+    </head>
+    <body>
+        <h1>Tambah data mahasiswa</h1>
+
+        <form action="" method="post">
+            <ul>
+
+                <li> 
+                  <label for="nama">Nama : </label>
+                  <input type="text" name="nama" id="nama">
+                </li>
+                <li>
+                    <label for="Pengaduan">Pengaduan : </label>
+                    <input type="text" name="Pengaduan" id="Pengaduan">
+                </li>
+                <li>
+                    <label for="gambar">Gambar : </label>
+                    <input type="text" name="gambar" id="gambar">
+                </li>
+                <li>
+                    <button type="submit" name="submit">Tambah Data!</button>
+                </li>
+            </ul>
+
+    </body>
+</html>
